bfdc26904aa383183029d4aa73f5b817b58bd157
izuizuizuko <izuizuizuko426@gmail.com>
2020-03-21 00:19:22 +0900

    qq

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do 
-  
-  resources :rests
-  resources :strains
   root 'first#index'
-  resources :questions
-  resources :top
-  resources :answers, :except => [:new]
-  resources :products 
-  resources :images 
-  resources :contacts 
-  resources :flowers 
-  resources :hordeolums
-  resources :kids
-  resources :reds
+ 
+  
+  resources :questions, only: [:new, :create, :show, :index, :edit, :update, :destroy ]
+  resources :top: [:new, :create, :show, :index, :edit, :update, :destroy ]
+  resources :answers: [:new, :create, :show, :index, :edit, :update, :destroy ]
+  resources :products : [:new, :create, :show, :index, :edit, :update, :destroy ]
+  resources :images : [:new, :create, :show, :index, :edit, :update, :destroy ]
+  resources :contacts : [:new, :create, :show, :index, :edit, :update, :destroy ]
+  resources :flowers : [:new, :create, :show, :index, :edit, :update, :destroy ]
+  resources :hordeolums: [:new, :create, :show, :index, :edit, :update, :destroy ]
+  resources :kids: [:new, :create, :show, :index, :edit, :update, :destroy ]
+  resources :reds: [:new, :create, :show, :index, :edit, :update, :destroy ]
+  resources :rests: [:new, :create, :show, :index, :edit, :update, :destroy ]
+  resources :strains: [:new, :create, :show, :index, :edit, :update, :destroy ]
   devise_for :users, controllers: {
       registrations: 'users/registrations',
       sessions: 'users/sessions'
