b29036a3e6cd5e83d243a0f258eb5181d03038c9
izuizuizuko <izuizuizuko426@gmail.com>
2020-03-20 17:31:50 +0900

    su

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do 
   root 'first#index'
-  resources :questions, only: [:new, :show, :index, :edit] 
   resources :top, only: [:new, :show, :index, :edit] 
+  resources :questions, only: [:new, :create, :show, :index, :edit] 
   resources :answers, :except => [:new]
-  resources :products , only: [:new, :show, :index, :edit] 
-  resources :images , only: [:new, :show, :index, :edit] 
-  resources :contacts , only: [:new, :show, :index, :edit] 
-  resources :flowers , only: [:new, :show, :index, :edit] 
-  resources :hordeolums, only: [:new, :show, :index, :edit] 
-  resources :kids, only: [:new, :show, :index, :edit] 
-  resources :reds, only: [:new, :show, :index, :edit] 
-  resources :rests, only: [:new, :show, :index, :edit] 
-  resources :strains, only: [:new, :show, :index, :edit] 
+  resources :products , only: [:new, :create, :show, :index, :edit] 
+  resources :images , only: [:new, :create, :show, :index, :edit] 
+  resources :contacts , only: [:new, :create, :show, :index, :edit] 
+  resources :flowers , only: [:new, :create, :show, :index, :edit] 
+  resources :hordeolums, only: [:new, :create, :show, :index, :edit] 
+  resources :kids, only: [:new, :show, :create, :index, :edit] 
+  resources :reds, only: [:new, :show, :create, :index, :edit] 
+  resources :rests, only: [:new, :show, :create, :index, :edit] 
+  resources :strains, only: [:new, :create, :show, :index, :edit] 
   devise_for :users, controllers: {
       registrations: 'users/registrations',
       sessions: 'users/sessions'
