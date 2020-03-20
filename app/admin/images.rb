94aaa7376f148affd6dddb3f4fe456bc82207a51
izuizuizuko <izuizuizuko426@gmail.com>
2020-03-20 20:32:39 +0900

    Update images.rb

--- app/admin/images.rb
+++ app/admin/images.rb
@@ -1,32 +1,28 @@
 ActiveAdmin.register Image do
-  
-  index do
-    selectable_column
-    id_column
-    column :image do |b|
-      image_tag(b.image.url(:thumb))
-    end
-    column :product_id
-    column :title
-    column :comoany
-    column :rank
-    column :point
-    column :updated_at
-    actions
-  end
-
-  show do |item_image|
-    attributes_table do
-      row :image do |b|
+ 
+  #indexページ
+    index do
+      selectable_column
+      id_column
+      column :image do |b|
         image_tag(b.image.url(:thumb))
       end
-      row :created_at
-      row :title
-      row :updated_at
-      row :product_id
-
+      column :product_id
+      column :created_at
+      column :updated_at
+      actions
     end
+   
+    #showページ
+    show do |image|
+      attributes_table do
+        row :image do |b|
+          image_tag(b.image.url(:thumb))
+        end
+        row :created_at
+        row :updated_at
+      end
+    end
+   
+    permit_params :image, :product_id
   end
- 
-  permit_params :image, :product_id, 
-end
