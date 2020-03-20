9cb9208084ea5bd55bc5789c1f81e31a888ff6da
izuizuizuko <izuizuizuko426@gmail.com>
2020-03-20 16:42:16 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-AdminUser.create!(email: 'admin@gmail.com', password: '11111111', password_confirmation: '11111111') if Rails.env.development?
+AdminUser.create!(email: 'admin@gmail.com', password: '11111111', password_confirmation: '11111111') if Rails.env.production?
